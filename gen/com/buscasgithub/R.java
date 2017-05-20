b805682abedd9fccef4d8aed123903d2a5e05e20
Caio Bartholomeu <caio.sadu@gmail.com>
2017-05-20 18:33:45 -0300

    Consumir Json

--- gen/com/buscasgithub/R.java
+++ gen/com/buscasgithub/R.java
@@ -22,10 +22,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int txtCpf=0x7f080001;
+        public static final int txtNome=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_informacoes=0x7f030000;
+        public static final int buscas_usuarios=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -34,9 +37,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int cpf=0x7f050005;
+        public static final int hello_world=0x7f050002;
+        public static final int nascimento=0x7f050004;
+        public static final int nome=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ src/com/buscasgithub/Pessoa.java
@@ -0,0 +1,30 @@
+package com.buscasgithub;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+	private String nome;
+	private String cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	@Override
+	public String toString() {
+		return  nome;
+	}
+
+}
